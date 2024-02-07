6353561ccc26b739c1c1a78a3bd5f2a5bb3c16f9
upadrashtav <vupadrashta@yahoo.com>
2024-02-06 21:04:13 -0600

    complete code

---
+++ src/main/java/com/example/springcloudclienteurekademo/SpringcloudclienteurekademoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudclienteurekademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudclienteurekademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudclienteurekademoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcloudclienteurekademo/SpringcloudclienteurekademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudclienteurekademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudclienteurekademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
